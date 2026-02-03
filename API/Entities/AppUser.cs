41a742d3b61366a9c4cacb482ec6f288792ca7a7
Keval Vora <vorakevin471@gmail.com>
2026-02-03 13:55:49 +0530

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -7,4 +7,8 @@ public class AppUser
     public required string DisplayName { get; set; }
 
     public required string Email { get; set; }
+
+    public byte[] PasswordHash { get; set; }
+
+    public byte[] PasswordSalt { get; set; }
 }
