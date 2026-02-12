c534eef9159f048ed0b4bb912580bd7a45349b63
Keval Vora <vorakevin471@gmail.com>
2026-02-12 11:57:56 +0530

    End of section 10

--- client/src/features/members/member-detailed/member-detailed.ts
+++ client/src/features/members/member-detailed/member-detailed.ts
@@ -1,30 +1,31 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { filter } from 'rxjs';
 import { Member } from '../../../types/member';
 import { AgePipe } from '../../../core/pipes/age-pipe';
+import { AccountService } from '../../../core/services/account-service';
+import { MemberService } from '../../../core/services/member-service';
 
 @Component({
   selector: 'app-member-detailed',
-  imports: [ RouterLink, RouterLinkActive, RouterOutlet, AgePipe],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, AgePipe],
   templateUrl: './member-detailed.html',
   styleUrl: './member-detailed.css',
 })
 export class MemberDetailed implements OnInit {
 
   private route = inject(ActivatedRoute);
+  protected memberService = inject(MemberService);
+  private accountService = inject(AccountService);
   private router = inject(Router);
-  protected member = signal<Member | undefined>(undefined);
   protected title = signal<string | undefined>('Profile');
 
-  ngOnInit(): void {
-    this.route.data.subscribe({
-      next: (data) =>{
-        this.member.set(data['member'])
-        console.log(this.member);
+  protected isCurrentUser = computed(() => {
+    return this.accountService.currentUser()?.id === this.route.snapshot.paramMap.get('id');
+  })
 
-      }
-    })
+  ngOnInit(): void {
+
     this.title.set(this.route.firstChild?.snapshot?.title);
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
@@ -32,7 +33,7 @@ export class MemberDetailed implements OnInit {
       next: () => {
         this.title.set(this.route.firstChild?.snapshot?.title)
       }
-    })  
+    })
   }
 
 }
